3722d74173d7c88253abb81fe4fe68872f7131c3
Yann Guichard <xloadx923@msn.com>
2022-06-22 15:00:34 +0200

    Modif media query

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ const button = document.querySelectorAll('.button');
 const allResume = document.querySelectorAll('.resume');
 
 function removeActive(){
-    allResume.forEach(function(element){    element.classList.remove('active');});
+    allResume.forEach(function(element){   element.classList.remove('active');});
 }
 
 /* Pour chaque bouton flèché */
 button.forEach(function(element){
     element.addEventListener('click', function(event){
         removeActive();
-        this.nextElementSibling.classList.toggle('active');
+        this.nextElementSibling.classList.add('active');
     });
 });
